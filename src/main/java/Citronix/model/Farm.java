d6a4fe9f02b9380c122871d4eba639e9c2cef6d0
zoolotoui <chag.med.amine@gmail.com>
2024-11-19 16:14:10 +0100

    stat: starting with first task

--- src/main/java/Citronix/model/Farm.java
+++ src/main/java/Citronix/model/Farm.java
@@ -1,18 +1,70 @@
 package Citronix.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
-@Entity
+@Entity(name = "farms")
 public class Farm {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
     private LocalDateTime created_at;
     private String location;
     private long superficie;
-    
 
+    public Farm(){
+
+    }
+
+    public Farm(String name, LocalDateTime created_at, String location, long superficie) {
+        this.name = name;
+        this.created_at = created_at;
+        this.location = location;
+        this.superficie = superficie;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public long getSuperficie() {
+        return superficie;
+    }
+
+    public void setSuperficie(long superficie) {
+        this.superficie = superficie;
+    }
 }
