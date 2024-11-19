260a59493779f222f43b428ad52db7dda90d065c
zoolotoui <chag.med.amine@gmail.com>
2024-11-19 16:57:25 +0100

    CIT-6 stat: working on update & delete

--- src/main/java/Citronix/controller/FarmController.java
+++ src/main/java/Citronix/controller/FarmController.java
@@ -3,6 +3,7 @@ package Citronix.controller;
 import Citronix.model.Farm;
 import Citronix.service.FarmServiceInterface;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +15,9 @@ public class FarmController {
     private final FarmServiceInterface farmService;
     @PostMapping
     public ResponseEntity<Farm> createFarm(@RequestBody Farm farm){
-
+        Farm frm = farmService.save(farm);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(frm);
     }
+
+
 }
--- src/main/java/Citronix/repository/FarmRepository.java
+++ src/main/java/Citronix/repository/FarmRepository.java
@@ -2,10 +2,10 @@ package Citronix.repository;
 
 import Citronix.model.Farm;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.UUID;
-@Repository
+
 public interface FarmRepository  extends JpaRepository<Farm, UUID> {
 
 }
